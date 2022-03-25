66cdb2cdc440c901b5c8d01625586a16c691d469
Khongthep <khongthepbme@gmail.com>
2022-03-25 14:42:24 +0700

    forth commit

--- src/testhello/TestHello.java
+++ src/testhello/TestHello.java
@@ -18,5 +18,6 @@ public class TestHello {
         System.out.println("Hello, World");    
         System.out.println("Welcome to Thailand");
         System.out.println("Have a Good Trip");
+        System.out.println("Wait a Minute");
     }    
 }
